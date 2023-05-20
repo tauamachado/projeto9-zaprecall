4e595ee449e2ebd12b2aa38b7f7ab302d0dfcd1d
Tauã Machado <taua.machado@estudante.ufjf.br>
2023-05-20 06:38:17 -0300

    Implementação flashcards - estado inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,79 @@
-import "./css/reset.css"
-import logo from "./assets/logo.png"
-import Questions from "./components/Questions"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import logo from "./assets/logo.png";
+import Questions from "./components/Questions";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const AppContainer = styled.div`
+  width: 375px;
+  height: 1000px;
+  background-color: #FB6B6B;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 42px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 51px;
+
+  h1 {
+    font-family: 'righteous';
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+    color: #FFFFFF;
+  }
+  
+  img {
+    width: 52px;
+    height: 60px;
+    padding-right: 20px;
+  }
+`;
+
+const Footer = styled.div`
+  width: 375px;
+  height: 70px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+
+  p {
+    font-family: 'Recursive';
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+  }
+`;
 
 export default function App() {
   return (
     <>
-      <AppStyled>
+      <GlobalStyle />
+      <AppContainer>
         <Header>
           <img src={logo} alt={logo} />
           <h1>ZapRecall</h1>
         </Header>
-      </AppStyled>
+        <Questions />
+      </AppContainer>
       <Footer>
         <p>0/4 CONCLUÍDOS</p>
       </Footer>
     </>
-  )
+  );
 }
-
-const AppStyled = styled.div`
-    width: 375px;
-    height: 1000px;
-    background-color: #FB6B6B;
-    display: flex;
-    align-items: center;
-    flex-direction: column;     
-    padding-top: 42px;
-`
-
-const Header = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 51px;
-
-    h1 {
-      font-family:'righteous';
-      font-weight: 400;
-      font-size: 36px;
-      line-height: 45px;
-      color: #FFFFFF;
-    }
-    
-    img {
-      width: 52px;
-      height: 60px;
-      padding-right: 20px;
-    }
-`
-
-const Footer = styled.div`
-    width: 375px;
-    height: 70px;
-    background-color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-
-    p {
-      font-family: 'Recursive';
-      font-weight: 400;
-      font-size: 18px;
-      line-height: 22px;
-    }
-`
-
-
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -12,6 +12,7 @@ const Pergunta = styled.li`
   align-items: center;
   padding: 0 22px 0 15px;
   margin-bottom: 25px;
+  margin-right: 38px;
   box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
 
   p {
@@ -21,16 +22,28 @@ const Pergunta = styled.li`
     line-height: 19px;
     color: #333333;
   }
-};
-`
+`;
+
+const QuestionsContainer = styled.ul`
+  list-style: none;
+`;
+
+const arrayPerguntas = [
+  { id: 1, texto: 'Pergunta 1' },
+  { id: 2, texto: 'Pergunta 2' },
+  { id: 3, texto: 'Pergunta 3' },
+  { id: 4, texto: 'Pergunta 4' },
+];
 
 export default function Questions() {
   return (
-    <ul>
-      <Pergunta>
-        <p>Pergunta 1</p>
-        <img src={seta_play} alt={seta_play} />
-      </Pergunta>
-    </ul>
+    <QuestionsContainer>
+      {arrayPerguntas.map((question) => (
+        <Pergunta key={question.id}>
+          <p>{question.texto}</p>
+          <img src={seta_play} alt={seta_play} />
+        </Pergunta>
+      ))}
+    </QuestionsContainer>
   );
 }
